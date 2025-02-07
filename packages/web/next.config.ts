f32ede9388333f27899fe1354a0bcd35b377ccfa
magnus <magnus@dahleide.com>
2025-02-07 15:47:34 +0100

    next config

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    nodeMiddleware: true,
+  },
 };
 
 export default nextConfig;
