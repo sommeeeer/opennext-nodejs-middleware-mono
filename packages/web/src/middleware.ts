3f02989401cc722074cb79d2772a68f680420aff
magnus <magnus@dahleide.com>
2025-02-11 23:03:32 +0100

    pnpm patch

--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -1,8 +1,14 @@
 import { NextResponse } from 'next/server';
 import crypto from 'node:crypto';
 
-export function middleware() {
+export async function middleware() {
   const response = NextResponse.next();
+  await new Promise<void>((resolve) =>
+    setTimeout(() => {
+      console.log('hihihi 1s');
+      resolve();
+    }, 1000)
+  );
 
   const randomString = crypto.randomBytes(16).toString('hex');
   response.headers.set('x-random-string', randomString);
